0e76b9014ae1c5c444d4fac45bce8d5fa980ab2d
YearaChoi <22200750@handong.ac.kr>
2024-06-25 20:53:11 +0900

    add: Themeprovider, recoil 및 키컬러 세팅

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
 import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
@@ -16,8 +18,22 @@ const router = createBrowserRouter([
   },
 ]);
 
+const theme = {
+  colors: {
+    purple300: "#ba68c8",
+    purple400: "#ab47bc",
+    purple500: "#9c27b0",
+    purple600: "#8e24aa",
+    purple700: "#7b1fa2",
+  },
+};
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
