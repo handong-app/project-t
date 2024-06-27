c1e35b23068c0c6446302990e26ce28ce4351767
LeeHannaa <103620720+LeeHannaa@users.noreply.github.com>
2024-06-27 19:40:41 +0900

    feat: 생성 페이지 기능 & 파베 데이터 저장 및 중복 코드 확인

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { auth } from "./../tools/firebase";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 function LoginPage() {
@@ -23,9 +24,17 @@ function LoginPage() {
   return (
     <div>
       <LoginBtn onClick={handleGoogleLogin}>구글 로그인</LoginBtn>
-      <Text>
-        {userData ? "환영합니다, " + userData.displayName + "님" : " "}
-      </Text>
+      {userData ? (
+        <>
+          <Text> 환영합니다, {userData.displayName}님 </Text>
+          //ToDo: 은주 : 너가 만든 페이지로 넘어가게 변경 (넘겨주면서 props같이)
+          <Link to="/create">
+            방 만들어보자 (은주가 작업한 페이지 메인에 올라오면 글로 옮길 예정)
+          </Link>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
