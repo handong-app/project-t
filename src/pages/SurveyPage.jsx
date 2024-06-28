6ee00447f5a89ea576d34e04d3e7bce780f7f537
junglesub <junglesubmarine@gmail.com>
2024-06-28 16:53:38 +0000

    feat: mode toggle

--- src/pages/SurveyPage.jsx
+++ src/pages/SurveyPage.jsx
@@ -2,6 +2,7 @@ import moment from "moment";
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 import MarkCalendar from "../components/MarkCalendar";
+import styled from "styled-components";
 
 const sampleData = [
   {
@@ -15,27 +16,55 @@ const sampleData = [
 ];
 
 function SurveyPage() {
+  const [editMode, setEditMode] = useState(false);
+  const [mySelectedDate, setMySelectedDate] = useState([]);
+
   const reservedDates = {};
   sampleData.map((markUserData) =>
     markUserData.date.map((date) => {
       reservedDates[date] = [...(reservedDates[date] || []), markUserData.id];
     })
   );
+
+  const toggleMode = () => {
+    setEditMode((prev) => !prev);
+  };
+
   console.log(reservedDates);
 
   return (
     <div>
       <h1>MarkingPage</h1>
-      <MarkCalendar
-        readOnly={true}
-        selectedDates={Object.keys(reservedDates)}
-      />
-      <MarkCalendar
-        readOnly={false}
-        selectedDates={Object.keys(reservedDates)}
-      />
+      {editMode ? (
+        <MarkCalendar
+          readOnly={false}
+          selectedDates={mySelectedDate}
+          setSelectedDate={setMySelectedDate}
+        />
+      ) : (
+        <MarkCalendar
+          readOnly={true}
+          selectedDates={Object.keys(reservedDates)}
+        />
+      )}
+
+      <ButtonContainer>
+        <Button onClick={toggleMode}>{editMode ? "저장" : "수정"}하기</Button>
+      </ButtonContainer>
     </div>
   );
 }
+
+const ButtonContainer = styled.div`
+  margin-top: 16px;
+  text-align: center;
+`;
+
+const Button = styled.button`
+  background-color: ${(props) =>
+    props.readOnly ? "grey" : props.theme.colors.purple400};
+  font-weight: bold;
+  color: white;
+`;
 
 export default SurveyPage;
