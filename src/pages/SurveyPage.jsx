94c3b908a0f49fc13bcb8952c4db448c7b1ee11b
junglesub <junglesubmarine@gmail.com>
2024-06-29 12:30:48 +0000

    feat: cal save/load from firestore

--- src/pages/SurveyPage.jsx
+++ src/pages/SurveyPage.jsx
@@ -1,36 +1,70 @@
 import moment from "moment";
-import React, { useState } from "react";
-import Calendar from "react-calendar";
+import React, { useEffect, useState } from "react";
 import MarkCalendar from "../components/MarkCalendar";
 import styled from "styled-components";
-
-const sampleData = [
-  {
-    id: "apple@handong.ac.kr",
-    date: ["2024-06-04", "2024-06-05", "2024-06-09"],
-  },
-  {
-    id: "22200750@handong.ac.kr",
-    date: ["2024-06-04", "2024-06-06", "2024-06-10"],
-  },
-];
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { auth, firestore } from "../tools/firebase";
+import { useParams } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { UserEmailState } from "../store/atom";
 
 function SurveyPage() {
+  const userEmail = useRecoilValue(UserEmailState);
+  const { displayName, email } = auth.currentUser;
+  const email_ = email.replaceAll(".", "_");
+
   const [editMode, setEditMode] = useState(false);
   const [mySelectedDate, setMySelectedDate] = useState([]);
 
+  const [roomInfo, setRoomInfo] = useState(null);
+
+  const { surveyId } = useParams();
+
+  const firebaseDoc = doc(firestore, "room", surveyId);
+
+  const toggleMode = async () => {
+    if (editMode) {
+      // 수정모드에서 저장.
+      await updateDoc(firebaseDoc, {
+        [`responsedata.${email_}`]: {
+          email,
+          displayName,
+          notAvalDates: mySelectedDate.map((date) =>
+            moment(date).format("YYYY-MM-DD")
+          ),
+        },
+      });
+      await getRoomInfo(); // 업데이트 되었기 때문에 새로운 정보 불러오기.
+      setEditMode(false);
+    } else {
+      setEditMode(true);
+    }
+  };
+
+  const getRoomInfo = async () => {
+    const roomDoc = (await getDoc(firebaseDoc)).data();
+    console.log(roomDoc);
+    setRoomInfo(roomDoc);
+
+    // 내가 선택한 날짜가 있으면 state 와 동기화 해주기
+    setMySelectedDate(
+      (roomDoc.responsedata && roomDoc.responsedata[email_]?.notAvalDates) || []
+    );
+  };
+
+  useEffect(() => {
+    getRoomInfo();
+  }, []);
+
+  if (!roomInfo) return <div>Loading Room Info...</div>;
+
+  // room info 가 로딩된 다음에 실행되야하는 것들
   const reservedDates = {};
-  sampleData.map((markUserData) =>
-    markUserData.date.map((date) => {
+  Object.values(roomInfo.responsedata || {}).map((markUserData) =>
+    markUserData.notAvalDates?.map((date) => {
       reservedDates[date] = [...(reservedDates[date] || []), markUserData.id];
     })
   );
-
-  const toggleMode = () => {
-    setEditMode((prev) => !prev);
-  };
-
-  console.log(reservedDates);
 
   return (
     <div>
