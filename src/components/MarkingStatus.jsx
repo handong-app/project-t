bc7fef53718acd28814699a4d91e2a31f68985a3
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-06-30 19:29:46 +0900

    feat : 모든 유저가 날짜 선택을 했을 경우 투표 페이지로 이동하는 버튼

--- src/components/MarkingStatus.jsx
+++ src/components/MarkingStatus.jsx
@@ -8,9 +8,24 @@ const MarkList = styled.div`
   margin-left: 50px;
 `;
 
+const AboveList = styled.div`
+  margin-bottom: 50px;
+`;
+
+const BottomList = styled.div`
+  margin-bottom: 50px;
+`;
+
+const VoteBtn = styled.button`
+  background-color: ${(props) => props.theme.colors.purple400};
+  color: white;
+`;
+
 function MarkingStatus({ userList }) {
   const [marked, setMarked] = useState([]);
   const [unMarked, setUnMarked] = useState([]);
+  const [goVote, setGoVote] = useState(true);
+
   useEffect(() => {
     const users = Object.keys(userList);
     const newMarked = [];
@@ -20,32 +35,42 @@ function MarkingStatus({ userList }) {
       // 유저 리스트의 유저의 선택한 날짜의 개수가 0 이라면
       if (userList[user].notAvalDates.length === 0) {
         newUnMarked.push(userList[user]);
+        setGoVote(false);
       } else {
         newMarked.push(userList[user]);
       }
+    }
+
+    if (newUnMarked.length == 0) {
+      setGoVote(true);
+    } else {
+      setGoVote(false);
     }
 
     setMarked(newMarked);
     setUnMarked(newUnMarked);
   }, [userList]);
 
-  console.log(marked, unMarked);
-
   return (
     <MarkList>
-      <div>
+      <AboveList>
         <h3>마킹 완료</h3>
-        {marked.map((user) => (
-          <div key={user.email}>{user.displayName}</div>
-        ))}
-      </div>
+        {marked.length == 0 ? (
+          <h5>모든 유저가 마킹을 하지 않음</h5>
+        ) : (
+          marked.map((user) => <div key={user.email}>{user.displayName}</div>)
+        )}
+      </AboveList>
 
-      <div>
+      <BottomList>
         <h3>기다리는 중...</h3>
-        {unMarked.map((user) => (
-          <div key={user.email}>{user.displayName}</div>
-        ))}
-      </div>
+        {unMarked.length == 0 ? (
+          <h5>모든 유저 마킹 완료</h5>
+        ) : (
+          unMarked.map((user) => <div key={user.email}>{user.displayName}</div>)
+        )}
+      </BottomList>
+      {goVote ? <VoteBtn>투표 시작</VoteBtn> : <></>}
     </MarkList>
   );
 }
