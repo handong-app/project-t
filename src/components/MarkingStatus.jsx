a529a9222c0292ada43dd0b040084dc908379c4e
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-06-30 16:56:01 +0900

    feat : 날짜 마킹 완료 유무에 따른 유저 분류 완료

--- src/components/MarkingStatus.jsx
+++ src/components/MarkingStatus.jsx
@@ -1,10 +1,53 @@
-// import styled from "styled-components";
+import styled from "styled-components";
+import { useEffect, useState } from "react";
 
-function MarkingStatus() {
-  <>
-    <div>마킹 완료</div>
-    <div>기다리는 중...</div>
-  </>;
+const MarkList = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 100px;
+  margin-left: 50px;
+`;
+
+function MarkingStatus({ userList }) {
+  const [marked, setMarked] = useState([]);
+  const [unMarked, setUnMarked] = useState([]);
+  useEffect(() => {
+    const users = Object.keys(userList);
+    const newMarked = [];
+    const newUnMarked = [];
+
+    for (const user of users) {
+      // 유저 리스트의 유저의 선택한 날짜의 개수가 0 이라면
+      if (userList[user].notAvalDates.length === 0) {
+        newUnMarked.push(userList[user]);
+      } else {
+        newMarked.push(userList[user]);
+      }
+    }
+
+    setMarked(newMarked);
+    setUnMarked(newUnMarked);
+  }, [userList]);
+
+  console.log(marked, unMarked);
+
+  return (
+    <MarkList>
+      <div>
+        <h3>마킹 완료</h3>
+        {marked.map((user) => (
+          <div key={user.email}>{user.displayName}</div>
+        ))}
+      </div>
+
+      <div>
+        <h3>기다리는 중...</h3>
+        {unMarked.map((user) => (
+          <div key={user.email}>{user.displayName}</div>
+        ))}
+      </div>
+    </MarkList>
+  );
 }
 
 export default MarkingStatus;
