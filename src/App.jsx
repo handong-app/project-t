8fff19be53acbd20f5151ae34f9e389c4609e398
ejPark43 <pej22200310@handong.ac.kr>
2024-07-06 11:55:52 +0900

    fix: 기본페이지를 MainPage로 설정, route /main -> / 로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,42 @@ import {
   signOut,
 } from "firebase/auth";
 import { Link } from "react-router-dom";
-import TestComponent from "./components/TestComponent";
-
+import MainPage from "./pages/MainPage";
+import styled from "styled-components";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Frontend!</h1>
-      <TestComponent />
-      <Link to="/test">To Test Page</Link> <br />
-      <Link to="/m/ABCDE">To Marking Page</Link> <br />
-      <div>
-        <button onClick={() => signOut(auth)}>로그아웃</button>
-      </div>
-    </div>
+    <AppContainer className="App">
+      <LogoutBtn>
+        <h1 className="title">Hello Frontend!</h1>
+        <button className="logout-button" onClick={() => signOut(auth)}>
+          로그아웃
+        </button>
+      </LogoutBtn>
+
+      <MainPage />
+    </AppContainer>
   );
 }
 
 export default App;
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const LogoutBtn = styled.div`
+  display: flex;
+  border-bottom: 2px solid ${(props) => props.theme.colors.purple700};
+  justify-content: space-between;
+  margin-bottom: 100px;
+  margin-top: 0px;
+
+  .title {
+    margin: 0;
+  }
+  .logout-button {
+    display: flex;
+    align-self: flex-end;
+    margin-bottom: 10px;
+    height: 40px;
+  }
+`;
